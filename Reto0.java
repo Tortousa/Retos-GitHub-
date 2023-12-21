3116255df12974af1201d0edd1cba9aae7280bf4
Tortousa <151463782+Tortousa@users.noreply.github.com>
2023-12-21 08:47:03 +0100

    Update Reto0.java

--- Reto0.java
+++ Reto0.java
@@ -6,17 +6,17 @@ cada impresion), sutituyendo los siguientes:
 -Multiplos de 3 y 5 a la vez por la palabra "frizzbuzz".
 */
 public class Reto0 {
-    public static void main(String[] args){
-        for(int i=1; i<=100; i++){
+	public static void main(String[] args){
+		for(int i=1; i<=100; i++){
 			if(i%3==0 && i%5==0){
-                System.out.println(i+" frizzbuzz");
-            }else if(i%3==0){
-                System.out.println(i+" frizz");
-            }else if(i%5==0){
-                System.out.println(i+" buzz");
-            }else {
-                System.out.println(i);
+				System.out.println(i+" frizzbuzz");
+			}else if(i%3==0){
+				System.out.println(i+" frizz");
+			}else if(i%5==0){
+				System.out.println(i+" buzz");
+			}else {
+				System.out.println(i);
 			}
 		}
-    }
+	}
 }
